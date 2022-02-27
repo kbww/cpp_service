b713b030205d6ca92cdb7945bb756af0d3d9316c
Irfan <irfnhm@gmail.com>
2022-02-27 10:52:36 +0530

    Fix lint

--- hello_crow/public/scripts/websocket.js
+++ hello_crow/public/scripts/websocket.js
@@ -1,33 +1,33 @@
-const url = location.origin.replace(/^http/, 'ws') + '/ws';
+const url = location.origin.replace(/^http/, "ws") + "/ws";
 const socket = new WebSocket(url);
-const $log = $('#log');
-const $msg = $('#msg');
-const $submit = $('#submitMessage');
+const $log = $("#log");
+const $msg = $("#msg");
+const $submit = $("#submitMessage");
 const id = Math.floor(Math.random() * 1000000);
 
-createMessage = (id, type, body) => JSON.stringify({id, type, body});
+createMessage = (id, type, body) => JSON.stringify({ id, type, body });
 
 socket.onopen = () => {
-    console.log('open');
+  console.log("open");
 };
 socket.onclose = () => {
-    console.log('close');
+  console.log("close");
 };
 socket.onerror = (e) => {
-    console.log('error', e);
+  console.log("error", e);
 };
 socket.onmessage = (e) => {
-    const message = JSON.parse(e.data);
-    switch (message.type) {
-        case 'chat':
-            $log.val(`${message.body}\n` + $log.val());
-            break;
-        default:
-            console.error('Unknown message type');
-    }
+  const message = JSON.parse(e.data);
+  switch (message.type) {
+    case "chat":
+      $log.val(`${message.body}\n` + $log.val());
+      break;
+    default:
+      console.error("Unknown message type");
+  }
 };
-$submit.submit(event => {
-    event.preventDefault();
-    socket.send(createMessage(id, 'chat', $msg.val()));
-    $msg.val('');
+$submit.submit((event) => {
+  event.preventDefault();
+  socket.send(createMessage(id, "chat", $msg.val()));
+  $msg.val("");
 });
